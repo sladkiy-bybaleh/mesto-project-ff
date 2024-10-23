d0969a9a90d7c09d50b1d54bbf445b52979af40a
Vladimir Piankov <vovchik-leon2@yandex.ru>
2024-10-23 12:16:31 +0300

    one

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/sladkiy-bybaleh/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template');
+const cardTemplateContent = cardTemplate.content;
+
 // @todo: DOM узлы
+
+const cardPlace = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
 
+function createCard(item) {
+  const cardTitle = item.name;
+  const cardLink = item.link;
+  const cardTemplateClone = cardTemplateContent.cloneNode(true);
+
+  cardTemplateClone.querySelector('.card__title').textContent = cardTitle;
+  cardTemplateClone.querySelector('.card__image').setAttribute('src', cardLink);
+  cardTemplateClone.querySelector('.card__image').setAttribute('alt', `Фотография - ${cardTitle}`);
+  const cardItem = cardTemplateClone.querySelector('.places__item');
+
+  const deleteButton = cardTemplateClone.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => deleteCard(cardItem));
+  return cardTemplateClone;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+  cardPlace.append(createCard(card));
+})
